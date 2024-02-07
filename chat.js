38010e8952bdf0f9075dc202718e3ec63e2818fd
lolpng944 <71849379+lolpng944@users.noreply.github.com>
2024-02-07 22:43:25 +0100

    Update chat.js

--- chat.js
+++ chat.js
@@ -61,7 +61,7 @@ async function joinGlobalChat(ws, token) {
     try {
       const expectedOrigin = "tw-editor://.";
       const response = await axios.get(
-        `https://51ea1a21-4526-499d-b3f7-2d46e00a3762-00-7py6a38m7qpi.riker.replit.dev/verify-token/${token}`,
+        `https://liquemgames-api.netlify.app/verify-token/${token}`,
         {
           headers: {
             Origin: expectedOrigin,
